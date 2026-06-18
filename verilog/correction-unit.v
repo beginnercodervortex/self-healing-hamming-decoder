db57cb60c554382a967d10bb7eabf02d867c5e99
Akshita Singh <akshita.nowhere@gmail.com>
2026-06-18 23:02:25 +0530

    Update correction-unit.v

--- verilog/correction-unit.v
+++ verilog/correction-unit.v
@@ -1,1 +1,17 @@
+module correction_unit(
+    input [6:0] codeword,
+    input [2:0] error_position,
+    output reg [6:0] corrected_codeword
+);
 
+always @(*) begin
+
+    corrected_codeword = codeword;
+
+    if(error_position != 0)
+        corrected_codeword[error_position-1]
+            = ~codeword[error_position-1];
+
+end
+
+endmodule
