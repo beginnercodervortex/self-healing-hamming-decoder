345631cb175dc5cd9fa39a54ec02287cbd725095
Akshita Singh <akshita.nowhere@gmail.com>
2026-06-18 23:11:23 +0530

    Update syndrome-generator.v

--- verilog/syndrome-generator.v
+++ verilog/syndrome-generator.v
@@ -1,24 +1,21 @@
-module testbench;
-
-reg [6:0] codeword;
-wire [2:0] syndrome;
-
-syndrome_generator uut (
-    .codeword(codeword),
-    .syndrome(syndrome)
+module syndrome_generator(
+    input [6:0] codeword,
+    output [2:0] syndrome
 );
 
-initial begin
+wire s1;
+wire s2;
+wire s4;
 
-    $display("------TEST 1------");
+// P1 checks positions 1,3,5,7
+assign s1 = codeword[0] ^ codeword[2] ^ codeword[4] ^ codeword[6];
 
-    codeword = 7'b0110111;;
+// P2 checks positions 2,3,6,7
+assign s2 = codeword[1] ^ codeword[2] ^ codeword[5] ^ codeword[6];
 
-    #10;
+// P4 checks positions 4,5,6,7
+assign s4 = codeword[3] ^ codeword[4] ^ codeword[5] ^ codeword[6];
 
-    $display("Codeword = %b", codeword);
-    $display("Syndrome = %b", syndrome);
-
-end
+assign syndrome = {s4,s2,s1};
 
 endmodule
