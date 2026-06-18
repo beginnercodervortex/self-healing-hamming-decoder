6b157fbe3b4b9b26fb8a7bc700a025a436db172b
Akshita Singh <akshita.nowhere@gmail.com>
2026-06-18 22:41:21 +0530

    Update tmr-voting.v

--- verilog/tmr-voting.v
+++ verilog/tmr-voting.v
@@ -1,1 +1,16 @@
+module tmr_majority_voter(
 
+    input [2:0] synA,
+    input [2:0] synB,
+    input [2:0] synC,
+
+    output [2:0] voted_syndrome
+
+);
+
+assign voted_syndrome =
+        (synA & synB) |
+        (synB & synC) |
+        (synA & synC);
+
+endmodule
