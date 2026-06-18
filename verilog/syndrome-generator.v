dafb1ff46fde91c3e33f9f143c093623b66d8337
Akshita Singh <akshita.nowhere@gmail.com>
2026-06-18 22:58:43 +0530

    Update syndrome-generator.v

--- verilog/syndrome-generator.v
+++ verilog/syndrome-generator.v
@@ -1,1 +1,24 @@
+module testbench;
 
+reg [6:0] codeword;
+wire [2:0] syndrome;
+
+syndrome_generator uut (
+    .codeword(codeword),
+    .syndrome(syndrome)
+);
+
+initial begin
+
+    $display("------TEST 1------");
+
+    codeword = 7'b0110111;;
+
+    #10;
+
+    $display("Codeword = %b", codeword);
+    $display("Syndrome = %b", syndrome);
+
+end
+
+endmodule
